55039d89cf1ca13a58ea83013bc01a3c9f301c86
suhara beevi <sairasuhara12345@gmail.com>
2023-06-02 13:41:39 +0530

    mongodb atlas setting commit

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config()
 const dbConnect = () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect("mongodb://127.0.0.1:27017/clothes", {
+    mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
 
       useUnifiedTopology: true,
